6a410df9765b4bcb1c20f3de5769b54ce821ff98
Ken <kengketa@gmail.com>
2021-04-20 21:24:15 +0700

    add ability

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'AdminLTE')
 
 @section('content_header')
-    <h1 class="m-0 text-dark">Dashboard</h1>
+    <h1 class="m-0 text-dark">Web development demo</h1>
 @stop
 
 @section('content')
@@ -11,7 +11,117 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <p class="mb-0">You are logged in!</p>
+                    This is a demo web development as required in the job description:
+                <h4>PHP Developer Job Description 1 positiion</h4>
+                <h4>Who is Traffic Log Pro:</h4>
+                <p>
+                    Traffic Log Pro is an industry leading Lead Manager/CRM and Call Center.  We offer our dealers a system that touts ease of use and the ability to stay engaged with customers and prospects before, during and after the sale.  Our applications allow dealers to take leads from multiple sources, including OEMs and website providers.  Dealers can communicate with customers and prospects via text and email.  Our Call Center makes calls on behalf of the dealer for Pre Purchase, Post Purchase and Service Completes to gather valuable information that will allow the dealer to better serve their customer base.
+                </p>
+                <h4>Responsibilities Include:</h4>
+                <table class="table table-bordered table-sm">
+                    <thead>
+                        <th style="width:40%">Job requirement</th>
+                        <th>My current ability</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Build new features on various web applications</td>
+                            <td class="table-success">I have developed/implemented various funtions and features using diferent libraries such as Fullcalendar,Line API, SMS API, Payment gateway API ect. </td>
+                        </tr>
+                        <tr>
+                            <td>Architect and build new web applications </td>
+                            <td class="table-success">I am able to build a web application from scratch and finish the project by my own.</td>
+                        </tr>
+                        <tr>
+                            <td>Scale, maintain and improve various existing codebases and systems infrastructures</td>
+                            <td class="table-success">I have a strong uderstanding on data and system structure, be able to design and adjust system workflow.</td>
+                        </tr>
+                        <tr>
+                            <td>Maintain and improve the development, staging and production environments of various applications</td>
+                            <td class="table-success">Always, Using git version control to proceed workflow and developing a project. </td>
+                        </tr>
+                        <tr>
+                            <td>Contribute to ongoing improvement of engineering practices and procedures</td>
+                            <td class="table-success">I normally work as a freelance, join in diference projects.</td>
+                        </tr>
+                        <tr>
+                            <td>Refactor older code base and improve efficiency and code base</td>
+                            <td class="table-success">I like to use libraries in a project I work on. Of course, I learnt a lot from using opensource libraries</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <ul>
+                </ul>
+                <h4>Skills Required:</h4>
+                <table class="table table-bordered table-sm">
+                    <thead>
+                        <th style="width:40%">Job requirement</th>
+                        <th>My current ability</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Excellent command of the English language is required</td>
+                            <td class="table-success">I used to study and live in Australia for 5 years. I can speak English as a second language.</td>
+                        </tr>
+                        <tr>
+                            <td>5+ years of experience in Web Development</td>
+                            <td class="table-success">I am familiar with website developing.I started creating website in 2010 using Dreamweaver,2013-2014: Wix, 2015: Wordpress, 2016:Native PHP, 2018:Laravel, 2020:VueJs </td>
+                        </tr>
+                        <tr>
+                            <td>5+ years of experience working with MySQL</td>
+                            <td class="table-success">I first touched Mysql in 2009 when I was in a Uni, Used alot in 2016 when I use Native PHP for developing projects</td>
+                        </tr>
+                        <tr>
+                            <td>Maintain and improve the development, staging and production environments of various applications</td>
+                            <td class="table-success">Always, Using git version control to proceed workflow and developing a project. </td>
+                        </tr>
+                        <tr>
+                            <td>At least 4 years of experience with Javascript, HTML5 and CSS3</td>
+                            <td class="table-warning">Although, I am not a javascript specialist but I can use VueJS which is a Javascript framework with my projects.</td>
+                        </tr>
+                        <tr>
+                            <td>Bachelor degree in Computer Science or Computer Engineering</td>
+                            <td class="table-success">I hold a Bachelor degree in Computer Engineering, KMUTT</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h4 class="mt-2">A good knowledge of any of the following would be an asset:</h4>
+                <table class="table table-bordered table-sm">
+                    <thead>
+                        <th style="width:40%">Job requirement</th>
+                        <th>My current ability</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>experience with CRM-like web applications</td>
+                            <td class="table-success">Yes, Developed CRM,HRM sofware</td>
+                        </tr>
+                        <tr>
+                            <td>Linux, Apache, MySQL server administration</td>
+                            <td class="table-success">Yes, This web applicaion is running on Linux,NGINX,MYSQL</td>
+                        </tr>
+                        <tr>
+                            <td>Object-Oriented programming</td>
+                            <td class="table-success">Yes, Laravel is an OOP</td>
+                        </tr>
+                        <tr>
+                            <td>Wordpress, Drupal, Laravel</td>
+                            <td class="table-success">familiar with Laravel and Wordpress</td>
+                        </tr>
+                        <tr>
+                            <td>Mobile web development</td>
+                            <td class="table-success">experienced in reponsive web development</td>
+                        </tr>
+                        <tr>
+                            <td>Azure</td>
+                            <td class="table-danger">Less experience with Azure</td>
+                        </tr>
+                        <tr>
+                            <td>Git</td>
+                            <td class="table-success">Yes, Experienced with git version controll</td>
+                        </tr>
+                    </tbody>
+                </table>
                 </div>
             </div>
         </div>
